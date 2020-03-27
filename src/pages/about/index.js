9721eddde19c5b9fddc03985529407ad06fe4765
Kendrick Arnett <kmarnett@gmail.com>
2020-03-26 21:42:51 -0500

    Add About content.

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -5,8 +5,8 @@ import Layout from '../../components/layout';
 import Image from '../../components/image';
 import SEO from '../../components/seo';
 import styled from '@emotion/styled';
-
-import { media } from '../../utils/rocketbelt';
+import { css } from '@emotion/core';
+import { media, fontSize } from '../../utils/rocketbelt';
 
 const Form = styled.form`
   width: 100%;
@@ -58,18 +58,89 @@ const FormWrapper = styled.section`
   justify-content: center;
 `;
 
+const Card = styled.section`
+  li {
+    margin-bottom: calc(1.666rem / 4);
+  }
+
+  ${media[1]} {
+    border: 1px solid var(--border-color-light);
+    display: inline-grid;
+    padding: 1rem;
+    grid-gap: 1rem;
+    grid-auto-rows: auto;
+    grid-template-areas: 'subhead' 'content';
+    flex-direction: column;
+    box-shadow: var(--box-shadow);
+    border-radius: var(--border-radius);
+    font-size: ${fontSize(-1)};
+
+    .overview_subhead {
+      font-size: ${fontSize(-1)};
+      margin: 0;
+    }
+
+    .overview_text {
+      margin-bottom: 0;
+    }
+
+    ul {
+      margin-bottom: 0;
+    }
+
+    p:last-of-type {
+      margin-bottom: 0;
+    }
+  }
+`;
+
 const AboutPage = () => (
   <Layout>
     <SEO title="Kendrick Arnett › About" />
     <h1>About Kendrick</h1>
     <p>
-      A developer-turned-designer and self-made handyman with a recently
-      reignited passion for camping, public lands, and national parks (they’re
-      America’s Best Idea!). A little snobby about coffee, beer, barbeque,
-      CSS-in-JS, participative leadership, and Oxford commas, but only because
-      they’re worth doing right. Striving to be the reader my librarian thinks I
-      am.
+      I'm a developer-turned-UXer who's enthralled with bridging the gap with
+      colleagues in both professions.
     </p>
+    <div
+      className="about_cards"
+      css={css`
+        display: grid;
+        grid-template-rows: auto;
+        grid-gap: 1rem;
+
+        ${media[1]} {
+          max-width: 640px;
+          grid-template-columns: 1fr 1fr;
+        }
+      `}
+    >
+      <Card>
+        <h2 className="overview_subhead">I'm Borderline Obsessed With…</h2>
+        <div className="overview_text">
+          <ul>
+            <li>Black holes</li>
+            <li>
+              Public lands & the National Park system (America's Best Idea!)
+            </li>
+            <li>Coffee</li>
+            <li>BBQ</li>
+          </ul>
+        </div>
+      </Card>
+
+      <Card>
+        <h2 className="overview_subhead">I Have Strong Opinions About…</h2>
+        <div className="overview_text">
+          <ul>
+            <li>Participative leadership</li>
+            <li>Oxford commas</li>
+            <li>CSS-in-JS</li>
+          </ul>
+          <p>… but only because they're worth doing right.</p>
+        </div>
+      </Card>
+    </div>
 
     <h2>Contact Kendrick</h2>
     <p>
